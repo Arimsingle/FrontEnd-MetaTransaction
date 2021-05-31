abd2d1d3b58282ba152b5e0ee9df5e90645d896d
Arimsingle <arim.coe60@gmail.com>
2021-06-01 00:04:00 +0700

    [Add] : add scss module

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
-import './App.css';
+import './sass/main.scss';
 
 function App() {
   return (
     <div className="App">
-      <h1>Meta Transaction Simulator</h1>
+      <h1 className="test">Meta Transaction Simulator</h1>
     </div>
   );
 }
