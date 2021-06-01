5a72694b6c76b183c9e8f512d104d763384b7e46
Arimsingle <arim.coe60@gmail.com>
2021-06-01 15:46:45 +0700

    [Add] : add card components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import { useSelector, useDispatch } from "react-redux";
 import Balance from "./components/Balance";
 import Navbar from "./components/Nav";
+import Card from "./components/Card";
 const App = () => {
   return (
     <div>
       <Balance />
       <Navbar />
+      <Card />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,24 @@
+import "../sass/components/card.scss"
+
 const Card = () => {
     return (
         <>
+            <div className="card">
+                <div className="card__background">
+                    <ul>
+                        <li>Signature</li>
+                        <li>Icon</li>
+                    </ul>
+                    <div className="input">
+                        <input placeholder="to" />
+                        <input placeholder="amount" />
+                        <input placeholder="nonce" />
+                    </div>
+                    <div className="button">
+                        <p>Transact</p>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
